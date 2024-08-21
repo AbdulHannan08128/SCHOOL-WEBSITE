f90d7514525596ba2feef772894a91814be64cfd
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-08-21 15:25:52 +0530

    V1

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Hero from '@/components/Hero/Hero';
+import Performers from '@/components/Performers/Performers';
+import Subjects from '@/components/Subjects/Subjects';
+import YearlyActivities from '@/components/Activities/Yearly/YearlyActivities';
+import Contact from '@/components/Contact/Contact';
+import Footer from '@/components/Footer/Footer';
 
 const Home = () => {
   return (
     <>
     <Hero/>
+    <Performers/>
+    <Subjects/>
+    <YearlyActivities/>
+    <Contact/>
+    <Footer/>
     </>
   )
 }
